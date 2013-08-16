ebcbbb7b09b54f0d707e071626409a84b787eeec
Norbert Melzer <timmelzer@gmail.com>
2013-08-16 11:14:23 +0200

    `hlr` treated as `haxelib run`

---
+++ haxe.plugin.zsh
@@ -0,0 +1,7 @@
+# Aliases
+alias   hl='haxelib'
+compdef hl=haxelib
+alias   hli='haxelib install'
+#compdef _haxelib hli=haxelib-install
+alias   hlr='haxelib run'
+compdef _haxelib hlr=haxelib-run
