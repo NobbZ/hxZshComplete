ebc3e05ac6d08fdfaf3871b2668bb945f0d298ae
Norbert Melzer <timmelzer@gmail.com>
2013-08-20 10:33:27 +0200

    .

--- haxe.plugin.zsh
+++ haxe.plugin.zsh
@@ -4,6 +4,9 @@ export HXCOMPLETEVERSION="0.1.0"
 alias   hl='haxelib'
 compdef hl=haxelib
 alias   hli='haxelib install'
-#compdef _haxelib hli=haxelib-install
+compdef _haxelib hli=haxelib-install
 alias   hlr='haxelib run'
 compdef _haxelib hlr=haxelib-run
+alias	hll='haxelib list'
+compdef _haxelib hll=haxelib-list
+
